469936153696e4656cea0a128a712a43e3fec062
Gustavo Silvério <gustavosk290905@gmail.com>
2023-03-24 11:27:46 -0300

    Criação Alteração de Task

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,9 +1,56 @@
-import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Modal, Button, InputGroup, Form } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { http } from "../Services/Provider";
+import Swal from "sweetalert2";
 
 
 const Modals = (props) => {
+
+    const tarefa = props.tarefaSelecionada[0];
+    const [titulo, setTitulo] = useState(tarefa.title);
+    const [desc, setDesc] = useState(tarefa.description);
+
+    const salvarAlterações  = async () => {
+        if(titulo){
+            await http.put("api/tarefas/atualizar-task/" + tarefa.id, {
+                title: titulo,
+                description: desc
+            }).then((res) => {
+                if(res){
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Tarefa alterada com sucesso!',
+                        showConfirmButton: false,
+                        timer: 2000,
+                        position: "top-end",
+                        toast: true,
+                        timerProgressBar: true,
+                        customClass: {
+                            timerProgressBar: "prog-bar-success"
+                        }
+                    })
+                    setTimeout(() => {
+                        window.location.reload(false);
+                    }, 2000);
+                }
+                props.onHide();
+            })
+        }else {
+            Swal.fire({
+                icon: 'error',
+                title: 'o título é obrigatório!',
+                showConfirmButton: false,
+                timer: 2000,
+                position: "top-end",
+                toast: true,
+                timerProgressBar: true,
+                customClass: {
+                    timerProgressBar: "prog-bar-error"
+                }
+            })
+        }
+    }
 
     return (
         <>
@@ -13,15 +60,37 @@ const Modals = (props) => {
                 keyboard={false}
                 centered
             >
-                <Modal.Header closeButton>
-                    <Modal.Title>{props.tarefaSelecionada[0].title}</Modal.Title>
-                </Modal.Header>
                 <Modal.Body>
-                    {props.tarefaSelecionada[0].description}
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Título</Form.Label>
+                            <Form.Control
+                                onChange={(e) => setTitulo(e.target.value)}
+                                type="text"
+                                placeholder={tarefa.title}
+                                autoFocus
+                            />
+                        </Form.Group>
+                        <Form.Group
+                            className="mb-3"
+                            controlId="exampleForm.ControlTextarea1"
+                        >
+                            <Form.Label>Descrição</Form.Label>
+                            <Form.Control
+                               onChange={(e) => setDesc(e.target.value)}
+                               placeholder={tarefa.description}
+                               as="textarea"
+                               rows={3} />
+                        </Form.Group>
+                    </Form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button onClick={props.editando} variant="primary">ALTERAR</Button>
-                    <Button onClick={props.onHide} variant="primary">OK</Button>
+                    <Button variant="secondary" onClick={props.onHide}>
+                        Fechar
+                    </Button>
+                    <Button variant="primary" onClick={() => salvarAlterações()}>
+                        Salvar alterações
+                    </Button>
                 </Modal.Footer>
             </Modal>
         </>
--- src/Pages/Tarefas.jsx
+++ src/Pages/Tarefas.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react';
 import './tarefas.css';
 import { http } from '../Services/Provider';
 import Swal from 'sweetalert2';
-import { Modal } from 'react-bootstrap';
-// import { Icon } from '@mdi/react';
-// import { mdiGithub } from '@mdi/js';
+import Modals from '../Components/Modal';
+import { Icon } from '@mdi/react';
+import { mdiGithub } from '@mdi/js';
 
 const Tarefas = () => {
 
@@ -13,15 +13,15 @@ const Tarefas = () => {
     const [desc, setDesc] = useState("");
     const [show, setShow] = useState(false);
     const [tarefaSelecionada, setTareafaSelecionada] = useState([]);
-    const [editar, setEditar] = useState(false);
-    const setModalShow = (id) =>
-    {
-        setTareafaSelecionada(tarefas.filter(t=> t.id == id))
+    const [obrigatorio, setObrigatorio] = useState(false);
+    const handleClose = () => setShow(false);
+    const setModalShow = (id) => {
+        setTareafaSelecionada(tarefas.filter(t => t.id == id))
         setShow(true);
     }
 
-     const getTarefas = async () => {
-       await http.get('api/tarefas/obter-todas').then((res)=> {
+    const getTarefas = async () => {
+        await http.get('api/tarefas/obter-todas').then((res) => {
             setTarefas(res.data);
         })
     }
@@ -29,16 +29,9 @@ const Tarefas = () => {
         getTarefas();
     }, [])
 
-    function editarf() {
-
-        setEditar(true);
-    }
-
-    // ESTA DANDO ERRO, ENTAO ARRUMAR
-
     async function novaTarefa() {
 
-        if(!titulo){
+        if (!titulo) {
             Swal.fire({
                 icon: 'error',
                 title: 'o título é obrigatório!',
@@ -53,9 +46,8 @@ const Tarefas = () => {
             })
 
             setObrigatorio(!obrigatorio);
-        }
-
-       await http.post('api/tarefas/criar-task', {
+        }else{
+        await http.post('api/tarefas/criar-task', {
             title: titulo,
             description: desc
         }).then(() => {
@@ -70,7 +62,6 @@ const Tarefas = () => {
                 customClass: {
                     timerProgressBar: "prog-bar-success"
                 }
-
             })
 
             setTimeout(() => {
@@ -80,10 +71,11 @@ const Tarefas = () => {
 
         setObrigatorio(!obrigatorio);
     }
+    }
 
     async function excluirTarefa(id) {
         await http.delete('api/tarefas/excluir-task/' + id).then((res) => {
-            if(res) {
+            if (res) {
                 Swal.fire({
                     icon: 'success',
                     title: 'Tarefa excluida com sucesso!',
@@ -102,21 +94,22 @@ const Tarefas = () => {
             }
         })
     }
-
-    const listaTarefas = tarefas.map(t => {
-        return <>
-            <li key={t.id} className='tarefa'>
-                <div className="txt">
-                    <h1 className='bgc'>{t.title}</h1>
-                    <p className='bgc'>{t.description}</p>
-                </div>
-                <div className="butts">
-                    <button onClick={() => setModalShow(t.id)} className='visu-but-task'>Visualizar</button>
-                    <button className='Excl-but-task'>Excluir</button>
-                </div>
-            </li>
-        </>
-    })
+    if (tarefas){
+        var listaTarefas = tarefas.map(t => {
+            return <>
+                <li key={t.id} className='tarefa'>
+                    <div className="txt">
+                        <h1 className='bgc'>{t.title}</h1>
+                        <p className='bgc'>{t.description}</p>
+                    </div>
+                    <div className="butts">
+                        <button onClick={() => setModalShow(t.id)} className='visu-but-task'>Visualizar</button>
+                        <button onClick={() => excluirTarefa(t.id)} className='Excl-but-task'>Excluir</button>
+                    </div>
+                </li>
+            </>
+        })
+    }
 
     return (
         <>
@@ -130,14 +123,14 @@ const Tarefas = () => {
                 <h1 className='titulo'>Minhas tarefas</h1>
                 <div className="add-tarefa">
                     <div className="titulo-camp">
-                        <label htmlFor="Titulo">Titulo { obrigatorio && <span>*</span>}</label>
+                        <label htmlFor="Titulo">Titulo {obrigatorio && <span>*</span>}</label>
                         <input onChange={(e) => setTitulo(e.target.value)} className='titulo-inp' type="text" />
                     </div>
                     <div className="descricao-camp">
                         <label htmlFor="Descrição">Descrição</label>
                         <input onChange={(e) => setDesc(e.target.value)} className='descricao-inp' type="text" />
-                    </div>
                     <button onClick={() => novaTarefa()} className='confirmar-but'>Adicionar</button>
+                    </div>
                 </div>
                 <div className="lista-de-tarefas">
                     <ul className='lista-das-tarefas'>
@@ -146,38 +139,13 @@ const Tarefas = () => {
                 </div>
             </div>
 
-            <div className="modal">
-            <Modal
-                show={show}
-                onHide={handleClose}
-                backdrop="static"
-                keyboard={false}
-                centered
-                >
-                <Modal.Header closeButton>
-                    {/* ALTERAR ESSA LOGICA ABAIXO */}
-                    <Modal.Title>{ idSelecionado !== 0 &&  tarefas[0].title}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    I will not close if you click outside me. Don't even try to press
-                    escape key.
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary">Understood</Button>
-                </Modal.Footer>
-            </Modal>
-            </div>
-
-            <Modals
-                show={show}
-                onHide={() => setShow(false)}
-                tarefaSelecionada={tarefaSelecionada}
-            />
-
-
+            { show && 
+                <Modals
+                    show={show}
+                    onHide={() => handleClose()}
+                    tarefaSelecionada={tarefaSelecionada}
+                />
+            }
         </>
     );
 }
